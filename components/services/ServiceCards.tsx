a91c88ecded1323bbac9f3573b6d548b65430ef9
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-11-05 13:47:27 +0600

    added slug path for services

--- components/services/ServiceCards.tsx
+++ components/services/ServiceCards.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const services = [
@@ -7,54 +8,63 @@ const services = [
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/physiotherapy.png",
     alt: "Physiotherapy",
+    slug: "physiotherapy",
   },
   {
     service: "Joint replacements",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/joint_replacements.png",
     alt: "Joint replacements",
+    slug: "joint_replacements",
   },
   {
     service: "Neurological Rehab",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/neurological_rehab.png",
     alt: "Neurological Rehab",
+    slug: "neurological_rehab",
   },
   {
     service: "Parkinson's Disease",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/parkinson_disease.png",
     alt: "Parkinson's Disease",
+    slug: "parkinson_disease",
   },
   {
-    service: "Pre- and Post-Operative Rehab",
+    service: "Pre and Post-Operative Rehab",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/operative_rehab.png",
     alt: "Pre- and Post-Operative Rehab",
+    slug: "Pre_and_Post-Operative_Rehab",
   },
   {
     service: "Rotator cuff surgery preparation",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/cuff_surgery.png",
     alt: "Rotator cuff surgery preparation",
+    slug: "rotator_cuff_surgery_preparation",
   },
   {
     service: "Spine and cosmetic surgeries",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/surgeries.png",
     alt: "Spine and cosmetic surgeries",
+    slug: "spine_and_cosmetic_surgeries",
   },
   {
     service: "Ligament and tendon repairs",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/repairs.png",
     alt: "Ligament and tendon repairs",
+    slug: "ligament_and_tendon_repairs",
   },
   {
     service: "Upper and lower extremities surgeries",
     text: "Lorem ipsum dolor sit amet, consectetuer adipiscing. Aenean commodo ligula eget dolor aenean massa cum sociis et natoque penatibus.",
     image: "/assets/services/extremities_surgeries.png",
     alt: "Upper and lower extremities surgeries",
+    slug: "upper_and_lower_extremities_surgeries",
   },
 ];
 
@@ -66,20 +76,22 @@ const ServiceCards = () => {
           className="bg-gradient-to-r from-[#0aa3e8] to-[#34e1d8] rounded-3xl p-[1px] z-20 "
           key={service.service}
         >
-          <article className="w-full h-[249px] border bg-white p-4 xl:p-8 rounded-3xl flex flex-col justify-center items-center border-transparent">
-            <Image
-              src={service.image}
-              alt={service.alt}
-              width={40}
-              height={40}
-              className="pb-3"
-            />
+          <Link href={`/services/${service.slug}`}>
+            <article className="w-full h-[249px] border bg-white p-4 xl:p-8 rounded-3xl flex flex-col justify-center items-center border-transparent">
+              <Image
+                src={service.image}
+                alt={service.alt}
+                width={40}
+                height={40}
+                className="pb-3"
+              />
 
-            <div className="text-xl font-bold  text-center">
-              {service.service}
-            </div>
-            <div className=" text-center pt-3 ">{service.text}</div>
-          </article>
+              <div className="text-xl font-bold  text-center">
+                {service.service}
+              </div>
+              <div className=" text-center pt-3 ">{service.text}</div>
+            </article>
+          </Link>
         </div>
       ))}
     </div>
