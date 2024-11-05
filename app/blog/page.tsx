cfa908c32bfef189275c2ab85003d94a50a9f32a
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-11-05 15:56:39 +0600

    added placeholder for blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>Blogs</div>;
+  return (
+    <article className="h-screen bg-white text-black ">
+      <div className="container text-center py-10 md:py-20">Blogs</div>
+    </article>
+  );
 };
 
 export default page;
