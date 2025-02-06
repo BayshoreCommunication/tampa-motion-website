15e0f60d3ad6a516def472a69c03bb2a88467f84
rahad-dev24 <rahad.dev24@gmail.com>
2025-02-06 13:00:26 +0600

    Update loading.js

--- app/loading.js
+++ app/loading.js
@@ -1,14 +1,14 @@
-import { Spinner } from '@nextui-org/react';
+import { Spinner } from "@nextui-org/react";
 
 export default function Loading() {
   return (
-    <div className='w-full h-[90vh] flex items-center justify-center'>
+    <div className="w-full h-[90vh] flex text-primary items-center justify-center bg-white">
       <Spinner
-        className='text-[#1B2639]'
-        label='Loading...'
-        color='default'
-        labelColor='foreground'
-        size='lg'
+        className="text-primary"
+        label="Loading..."
+        color="primary"
+        labelColor="primary"
+        size="lg"
       />
     </div>
   );
