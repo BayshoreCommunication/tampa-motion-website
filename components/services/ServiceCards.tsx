de0f086a2d0ae9d5004bdd1df874918f19a8d677
rahad-dev24 <rahad.dev24@gmail.com>
2025-02-20 11:53:12 +0600

    update

--- components/services/ServiceCards.tsx
+++ components/services/ServiceCards.tsx
@@ -5,16 +5,32 @@ import ScrollMotionEffect from "@/components/motion/ScrollMotionEffect";
 import { serviceMenu, services } from "@/config/serviceData.js";
 
 const ServiceCards = () => {
-  serviceMenu.filter((i) => i.service == "Manual Lymphatic Drainage");
+  let filtered = [];
+  function customeFilter(arr: any, condition: any) {
+    return arr.reduce((newArr: any, item: any) => {
+      if (condition(item)) {
+        newArr.push(item);
+      }
+      return newArr;
+    }, []);
+  }
+  filtered = customeFilter(
+    serviceMenu,
+    (item: any) => item.service !== "Manual Lymphatic Drainage"
+  );
+  filtered = customeFilter(
+    filtered,
+    (item: any) => item.service !== "Pelvic Floor Strengthening"
+  );
   return (
     <div className=" h-fit inline-grid gap-3 2xl:gap-12 grid-flow-row grid-cols-1 w-full  items-center auto-rows-min py-10 lg:py-20 px-5  text-black  ">
-      {serviceMenu.map((service, index) => (
+      {filtered.map((service: any, index: any) => (
         <ScrollMotionEffect effect="fade-up" duration={2000} key={index}>
           <div
             className="bg-gradient-to-r from-[#0aa3e8] to-[#34e1d8] rounded-3xl p-[2px] z-20 "
             key={service.service}
           >
-            <article className="w-full   border bg-white p-4 xl:p-8 rounded-3xl flex flex-col justify-center items-center border-transparent">
+            <article className="w-full   border bg-[#34e1d8]/20 p-4 xl:p-8 rounded-3xl flex flex-col justify-center items-center border-transparent">
               {/* <Image
                 src={
                   service.service
@@ -27,11 +43,11 @@ const ServiceCards = () => {
                 className="pb-3"
               /> */}
 
-              <p className="text-4xl font-bold  pb-10 text-center">
+              <p className="text-4xl font-bold  pb-10 xl:pb-16 text-center">
                 {service.service}
               </p>
               <div className=" grid grid-flow-row grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5  w-full justify-center items-center align-middle content-center auto-rows-min   text-black  ">
-                {service.subClass.map((item, i) => (
+                {service.subClass.map((item: any, i: any) => (
                   <div
                     key={i}
                     className="bg-gradient-to-r from-[#0aa3e8] to-[#34e1d8] rounded-3xl p-[2px] z-20 hover:scale-105  transition-all duration-300 ease-in-out"
@@ -40,16 +56,20 @@ const ServiceCards = () => {
                       <article className="w-full  h-[376px]  lg:h-[340px] border bg-white p-4 xl:p-8 rounded-3xl flex flex-col gap-5 justify-center items-center border-transparent">
                         <Image
                           src={
-                            services.filter((i) => {
-                              i.service_slug === item.service_slug;
-                              return i.image;
-                            })[i]?.image
+                            customeFilter(
+                              services,
+                              (i: any) =>
+                                `${i.slug.toLowerCase()}` ==
+                                `${item.service_slug.toLowerCase()}`
+                            )[0]?.image
                           }
                           alt={
-                            services.filter((i) => {
-                              i.service_slug === item.service_slug;
-                              return i.alt;
-                            })[i]?.alt
+                            customeFilter(
+                              services,
+                              (i: any) =>
+                                `${i.slug.toLowerCase()}` ==
+                                `${item.service_slug.toLowerCase()}`
+                            )[0]?.alt
                           }
                           width={40}
                           height={40}
@@ -57,20 +77,24 @@ const ServiceCards = () => {
                         />
 
                         <p className="text-xl font-bold  text-center">
-                          {
+                          {/* {
                             services.filter((i) => {
                               i.service_slug === item.service_slug;
                               return i.service;
                             })[i].service
-                          }
+                          } */}
+                          {item.service}
                         </p>
                         <p className=" text-center pt-3  line-clamp-5">
                           {
-                            services.filter((i) => {
-                              i.service_slug === item.service_slug;
-                              return i.text;
-                            })[i]?.text
+                            customeFilter(
+                              services,
+                              (i: any) =>
+                                `${i.slug.toLowerCase()}` ==
+                                `${item.service_slug.toLowerCase()}`
+                            )[0]?.text
                           }
+                          {/* {item.text} */}
                         </p>
                       </article>
                     </Link>
