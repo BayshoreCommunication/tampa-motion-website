5928bceb96d1a2fac74dd81782a1944987de20bf
arsahak <arsahak99@gmail.com>
2025-09-02 17:54:12 +0600

    update

--- next.config.js
+++ next.config.js
@@ -1,20 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // Move this outside the remotePatterns array
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "bayshore.nyc3.digitaloceanspaces.com",
-        // hostname: "**",
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
       },
       {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'drive.google.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bayshore.nyc3.digitaloceanspaces.com',
       },
     ],
   },
+  skipTrailingSlashRedirect: true,
 };
 
 module.exports = nextConfig;
