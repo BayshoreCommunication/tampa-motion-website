56b2752fc75fdb81151cfe21266f0f442b5d3a64
arsahak <arsahak99@gmail.com>
2025-09-02 17:51:30 +0600

    update blog card

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,10 +1,11 @@
-import React from "react";
-import Head from "next/head";
-import PageHeroSection from "@/components/shared/PageHeroSection";
-import GetAllPostData from "@/lib/GetBlogDataAction";
-import ScrollMotionEffect from "@/components/motion/ScrollMotionEffect";
+import parse from "html-react-parser";
 import Image from "next/image";
 import Link from "next/link";
+import React from "react";
+
+import ScrollMotionEffect from "@/components/motion/ScrollMotionEffect";
+import PageHeroSection from "@/components/shared/PageHeroSection";
+import GetAllPostData from "@/lib/GetBlogDataAction";
 
 export async function generateMetadata({ params }) {
   return {
@@ -36,38 +37,38 @@ const page = async () => {
   return (
     <section className="w-full flex flex-col justify-start items-center min-h-screen bg-white">
       <PageHeroSection
-        title="Blogs"
         description="Welcome to our blog. Here, we share expert advice, tips, and success stories to help you on your journey to a healthy life."
+        title="Blogs"
       />
       <article className="bg-white w-full ">
         <div className="container bg-white grid grid-flow-row auto-rows-max grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 py-10">
           {posts?.map((el, index) => (
-            <ScrollMotionEffect effect="fade-up" duration={2000} key={index}>
-              <div className=" bg-white !shadow-md !rounded-md border h-full hover:scale-105 transition-all duration-400 ease-in-out">
+            <ScrollMotionEffect key={index} duration={2000} effect="fade-up">
+              <Link
+                className=" bg-white !shadow-md !rounded-md border h-full hover:scale-105 transition-all duration-400 ease-in-out"
+                href={`/blog/${el?.slug}`}
+              >
                 <Image
-                  src={el?.featuredImage?.image?.url}
                   alt="Custom Icon"
-                  width={500}
+                  className="mx-auto bg-cover bg-center"
                   height={400}
-                  className="mx-auto h-[400px]"
+                  src={el?.featuredImage?.image?.url}
+                  width={500}
                 />
                 <div className="pt-2 pb-8 px-4">
                   <h1 className="font-semibold  text-lg text-black text-center md:text-left mt-5">
                     {el?.title}
                   </h1>
                   <p className="text-base text-black text-center md:text-left mt-2 line-clamp-2 ">
-                    {el?.shortTitle}
+                    {parse(el?.body || "")}
                   </p>
                   <div className="mt-8">
-                    <Link
-                      href={`/blog/${el?.slug}`}
-                      className="text-secondary font-normal text-lg bg-none px-4 py-2 rounded-full hover:bg-secondary hover:text-white border-2 border-secondary  "
-                    >
+                    <button className="text-secondary font-normal text-lg bg-none px-4 py-2 rounded-full hover:bg-secondary hover:text-white border-2 border-secondary  ">
                       Read More
-                    </Link>
+                    </button>
                   </div>
                 </div>
-              </div>
+              </Link>
             </ScrollMotionEffect>
           ))}
         </div>
